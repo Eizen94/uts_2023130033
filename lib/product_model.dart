f0a73146b76b9bf9a00f736a421ae27522dc6266
Eizen94 <klausi_lanthaler@outlook.com>
2024-10-23 03:33:44 +0700

    Project untuk UTS PPB - Nikolaus Franz - 2023130033

--- lib/product_model.dart
+++ lib/product_model.dart
@@ -15,75 +15,188 @@ class Product {
 //Product Data
 List<Product> sampleProducts = [
   Product(
-      name: 'Sony - Playstation 5',
-      price: 8900000.00,
-      imageUrl: 'assets/images/products/SonyPS5.jpg',
-      description: ''),
+    name: 'Sony - Playstation 5',
+    price: 8900000.00,
+    imageUrl: 'assets/images/products/SonyPS5.jpg',
+    description:
+        '''The PS5® console unleashes new gaming possibilities that you never anticipated.
+
+Release Date : 20 February 2024
+
+Experience lightning fast loading with an ultra-high speed SSD, deeper immersion with support for haptic feedback, adaptive triggers, and 3D Audio, and an all-new generation of incredible PlayStation® games.
+
+1TB of Storage
+Keep your favorite games ready and waiting for you to jump in and play with 1TB of SSD storage built in.
+
+Ultra-High Speed SSD
+Maximize your play sessions with near instant load times for installed PS5® games.
+
+Integrated I/O
+The custom integration of the PS5® console's systems lets creators pull data from the SSD so quickly that they can design games in ways never before possible.
+
+What's in the Box
+
+➔ PlayStation®5 console
+➔ DualSense™ Wireless Controller
+➔ 1TB SSD
+➔ 2 Horizontal stand feet
+➔ HDMI® cable
+➔ AC power cord
+➔ USB cable
+➔ Printed materials
+
+ASTRO's PLAYROOM (Pre-installed game)''',
+  ),
   Product(
-      name: 'Sony - Playstation 5 Pro',
-      price: 14999000.00,
-      imageUrl: 'assets/images/products/SonyPS5_Pro.jpg',
-      description: ''),
+    name: 'Sony - Playstation 5 Pro',
+    price: 14999000.00,
+    imageUrl: 'assets/images/products/SonyPS5_Pro.jpg',
+    description: '''Witness Play Unleashed™
+
+With the PlayStation®5 Pro console, the world’s greatest game creators can enhance their games with incredible features like advanced ray tracing, super sharp image clarity for your 4K TV, and high frame rate gameplay.
+
+PlayStation® Spectral Super Resolution (PSSR)
+Get super sharp image clarity on your 4K TV using AI-enhanced resolution for ultra-high definition play with astonishing detail.
+
+Optimized Console Performance
+Reach higher and more consistent frame rates for silky smooth gameplay with support for 60Hz and 120Hz displays.
+
+PS5 Pro enhanced games
+Experience these incredible PS5 titles, enhanced with the power of PS5 Pro for improved visuals and performance.
+
+Play at 60fps, or up to 120fps, with 4K output and ray tracing for compatible games
+With the power of the PS5 Pro console, compatible games can be played at 60fps, or up to 120fps, with Ray Tracing and AI-enhanced 4K resolution using PSSR—all at the same time—on your 4K TV.
+
+2TB of storage
+Keep your favorite games ready and waiting for you to jump in and play with 2TB of SSD storage built-in.
+
+Next-level online wireless connectivity
+PS5 Pro supports enhanced wireless internet capabilities with more usable bandwidth to increase transfer speeds with IEEE 802.11be and your compatible router, which means you can experience reduced latency and boosted stability when playing online.
+
+PS5 Pro Game Boost and backwards compatibility
+The PS5 Pro console can play over 8,500 PS4® games. With the PS5 Pro version of the Game Boost feature, you can enjoy faster and smoother frame rates in some of the PS4 and PS5 console’s greatest games.''',
+  ),
   Product(
-      name: 'Microsoft - Xbox Series S',
-      price: 4490000.00,
-      imageUrl: 'assets/images/products/Xbox Series S.jpg',
-      description: ''),
+    name: 'Microsoft - Xbox Series S',
+    price: 4490000.00,
+    imageUrl: 'assets/images/products/Xbox Series S.jpg',
+    description:
+        '''The next generation of gaming brings our largest digital launch library yet to our smallest Xbox ever. With more dynamic worlds, faster load times, and the addition of Xbox Game Pass (sold separately), the all-digital Xbox Series S is the best value in gaming.
+Experience next-gen speed and performance with Xbox Series S in Carbon Black, featuring a 1TB SSD.
+
+Content Packages
+➔ 1x Xbox Series S console
+➔ 1x Xbox Wireless Controller
+➔ 1x High Speed HDMI cable
+➔ 1x Cable Power
+➔ 2x Pcs Baterai A2''',
+  ),
   Product(
-      name: 'Microsoft - Xbox Series X',
-      price: 7125000.00,
-      imageUrl: 'assets/images/products/Xbox Series X.jpg',
-      description: ''),
+    name: 'Microsoft - Xbox Series X',
+    price: 7125000.00,
+    imageUrl: 'assets/images/products/Xbox Series X.jpg',
+    description: '''Spesifikasi: 
+
+12 Teraflops
+Variable Rate Shading
+Hardware Accelerated DirectX Raytracing
+Quick Resume for Multiple Games
+Smart Delivery
+Native 4K, 8K Support, Up to 120 fps
+Custom AMD Zen 2 (8 Core)
+Custom AMD Radeon RNDA Navi
+NVMe SSD Drive
+Series X Controller (Includes Share Button and hybrid D-Pad), Xbox One Controller Compatible
+Include Auto Low Latency Mode (ALLM) and Dynamic Latency Input (DLI) that will make Xbox Series X the most responsive console ever
+Native 4K, 8K Support, Up to 120 fps
+Custom AMD Zen 2 (8 Core)
+Custom AMD Radeon RNDA Navi
+NVMe SSD Drive
+Series X Controller (Includes Share Button and hybrid D-Pad), Xbox One Controller Compatible
+Include Auto Low Latency Mode (ALLM) and Dynamic Latency Input (DLI) that will make Xbox Series X the most responsive console ever''',
+  ),
   Product(
-      name: 'Nintendo - Switch',
-      price: 3400000.00,
-      imageUrl: 'assets/images/products/Nintendo Switch.jpg',
-      description: ''),
+    name: 'Nintendo - Switch',
+    price: 3400000.00,
+    imageUrl: 'assets/images/products/Nintendo Switch.jpg',
+    description: '''Nintendo Switch - Neon Blue & Red
+
+Model number: HAC-001(-01) (product serial number begins with “XKW-XKJ”)
+
+Content:
+➔ Nintendo Switch Console Neon Blue & Red x1
+➔ Nintendo Switch Dock x1
+➔ Joy-Con (L) & (R)
+➔ Joy-Con Strap x2
+➔ Joy-Con Grip x1
+➔ High Speed HDMI Cable x1
+➔ Nintendo Switch AC Adapter x1
+
+Nintendo Switch + Free Temper Glass''',
+  ),
   Product(
-      name: 'Nintendo - Switch - OLED',
-      price: 3990000.00,
-      imageUrl: 'assets/images/products/Nintendo Switch OLED.jpg',
-      description: ''),
+    name: 'Nintendo - Switch - OLED',
+    price: 3990000.00,
+    imageUrl: 'assets/images/products/Nintendo Switch OLED.jpg',
+    description:
+        '''Nintendo Switch Console OLED merupakan generasi Nintendo Switch terbaru dengan ukuran layar lebih besar 7 Inch dan dilengkapi dengan internal storage 64GB yang lebih besar dari variant sebelumnya.
+
+Include :
+➔ Nintendo Switch Console
+➔ Nintendo Switch Dock (White)
+➔ Joy-Con (L) White
+➔ Joy-Con (R) White
+➔ Joy-Con Strap Accessories
+➔ Joy Con Grip
+➔ HDMI Cable
+➔ Nintendo Switch AC adapter
+
+Features white Joy-Con controllers, a black console, and a white dock.''',
+  ),
   Product(
     name: 'MSI - Claw Ultra 7 16/1TB',
     price: 8249000.00,
     imageUrl: 'assets/images/products/MSI - Claw Ultra 7 16GB - 1TB.jpg',
     description: '''Spesifikasi:
-        Intel® Core™ Ultra 7 Processor with Intel® AI Boost (NPU)
-        Windows 11 Home
-        7" FHD (1920x1080), 120Hz, IPS-Level, Touchscreen Intel® Arc™ Graphics
-        16GB LPDDR5-6400
-        1TB SSD
-        Intel® Killer™ BE Wi-Fi 7 + Bluetooth 5.4
+
+Intel® Core™ Ultra 7 Processor with Intel® AI Boost (NPU)
+
+Windows 11 Home
+
+7" FHD (1920x1080), 120Hz, IPS-Level, Touchscreen Intel® Arc™ Graphics
+
+16GB LPDDR5-6400
+1TB SSD
+Intel® Killer™ BE Wi-Fi 7 + Bluetooth 5.4
         
-        AUDIO JACK
-        1x Mic-in/Headphone-out Combo Jack
+AUDIO JACK
+1x Mic-in/Headphone-out Combo Jack
         
-        I/O PORTS
-        1x Type-C (USB / DP / Thunderbolt™ 4) with PD charging
-        1x Micro SD Card Reader
+I/O PORTS
+1x Type-C (USB / DP / Thunderbolt™ 4) with PD charging
+1x Micro SD Card Reader
         
-        CONTROL
-        RGB ABXY buttons
-        RGB L & R Hall Effect analog sticks
-        D-pad
-        L & R Hall Effect analog triggers
-        L & R bumpers
-        View button
-        Menu button
-        MSI Center M button
-        Quick Settings Key
-        2x Assignable grip buttons
-        HD haptics
+CONTROL
+RGB ABXY buttons
+RGB L & R Hall Effect analog sticks
+D-pad
+L & R Hall Effect analog triggers
+L & R bumpers
+View button
+Menu button
+MSI Center M button
+Quick Settings Key
+2x Assignable grip buttons
+HD haptics
         
-        Fingerprint Security
-        6-Cell 53 Battery (Whr)
-        AC ADAPTER 65W PD adapter
+Fingerprint Security
+6-Cell 53 Battery (Whr)
+AC ADAPTER 65W PD adapter
         
-        DIMENSION (WXDXH) : 294 x 117 x 21.2mm
-        WEIGHT (W/ BATTERY) : 675 g
+DIMENSION (WXDXH) : 294 x 117 x 21.2mm
+WEIGHT (W/ BATTERY) : 675 g
         
-        COLOR : Black''',
+COLOR : Black''',
   ),
   Product(
     name: 'Asus - ROG Ally X AMD Z1 Extreme 24G/1TB',
