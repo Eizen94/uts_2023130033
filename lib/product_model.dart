7d481786fdb62a93b3b7b0d73f5ccc6bd1f203d2
Eizen94 <klausi_lanthaler@outlook.com>
2024-10-22 23:29:25 +0700

    Project untuk UTS PPB - Nikolaus Franz - 2023130033

--- lib/product_model.dart
+++ lib/product_model.dart
@@ -43,17 +43,6 @@ List<Product> sampleProducts = [
     imageUrl: 'assets/images/products/Nintendo Switch OLED.jpg',
   ),
   Product(
-    name: 'Xiaomi - Pad 6s Pro 12,4 inch 8/256GB',
-    price: 7582000.00,
-    imageUrl:
-        'assets/images/products/Xiaomi Pad 6s Pro 12,4 inch 8GB - 256GB.jpg',
-  ),
-  Product(
-    name: 'Samsung - Galaxy Tab S9+ 12/512GB',
-    price: 19999000.00,
-    imageUrl: 'assets/images/products/Samsung Galaxy Tab S9+ 12GB - 512GB.jpg',
-  ),
-  Product(
     name: 'MSI - Claw Ultra 7 16/1TB',
     price: 8249000.00,
     imageUrl: 'assets/images/products/MSI - Claw Ultra 7 16GB - 1TB.jpg',
@@ -63,5 +52,16 @@ List<Product> sampleProducts = [
     price: 13999000.00,
     imageUrl:
         'assets/images/products/Asus - ROG Ally X AMD Z1 Extreme 24GB - 1TB.jpg',
+  ),
+  Product(
+    name: 'Xiaomi - Pad 6s Pro 12,4 inch 8/256GB',
+    price: 7582000.00,
+    imageUrl:
+        'assets/images/products/Xiaomi Pad 6s Pro 12,4 inch 8GB - 256GB.jpg',
+  ),
+  Product(
+    name: 'Samsung - Galaxy Tab S9+ 12/512GB',
+    price: 19999000.00,
+    imageUrl: 'assets/images/products/Samsung Galaxy Tab S9+ 12GB - 512GB.jpg',
   ),
 ];
