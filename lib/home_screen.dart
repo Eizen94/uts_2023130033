3691ed5ea6adfec0b43e6c048b6cd9195e5edb1f
Eizen94 <klausi_lanthaler@outlook.com>
2024-10-22 19:56:42 +0700

    Project untuk UTS PPB - Nikolaus Franz - 2023130033

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -6,16 +6,28 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-      body: Center(
-        child: Text(
-          'Welcome to the Home Screen!',
-          style: Theme.of(context).textTheme.headlineSmall,
-        ),
-      ),
-    );
+        body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [Color(0xFFE0C3FC), Color(0xFF8EC5FC)],
+              ),
+            ),
+            child: SafeArea(
+              child: Column(children: [
+                Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.menu, color: Colors.white),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ))
+              ]),
+            )));
   }
 }
