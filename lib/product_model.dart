fab323c633e9c5db5251b83bcc57730c6d739900
Eizen94 <klausi_lanthaler@outlook.com>
2024-10-22 23:08:58 +0700

    Project untuk UTS PPB - Nikolaus Franz - 2023130033

--- lib/product_model.dart
+++ lib/product_model.dart
@@ -42,4 +42,15 @@ List<Product> sampleProducts = [
     price: 3990000.00,
     imageUrl: 'assets/images/products/Nintendo Switch OLED.jpg',
   ),
+  Product(
+    name: 'Xiaomi Pad 6s Pro 12,4 inch 8/256GB',
+    price: 7582000.00,
+    imageUrl:
+        'assets/images/products/Xiaomi Pad 6s Pro 12,4 inch 8GB - 256GB.jpg',
+  ),
+  Product(
+    name: 'Samsung Galaxy Tab S9+ 12/512GB',
+    price: 19999000.00,
+    imageUrl: 'assets/images/products/Samsung Galaxy Tab S9+ 12GB - 512GB.jpg',
+  ),
 ];
